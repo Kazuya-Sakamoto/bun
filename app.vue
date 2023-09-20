ba163ae0d0433edd8984c8f32a77fcc768b9d897
坂本一弥 <sakamotokazuya@sakamotokazuyanoMacBook-Pro.local>
2023-09-20 21:52:05 +0900

    init

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script setup>
+const items = new Array(1000000).fill({ id: 0, name: "Item" });
+</script>
+
+<template>
+  <div>
+    <NuxtWelcome />
+    <div>
+      <div v-for="item in items" :key="item.id">
+        <!-- Content here -->
+      </div>
+    </div>
+  </div>
+</template>
